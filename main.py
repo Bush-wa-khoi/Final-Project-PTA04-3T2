404a12132355df00491d4ba77e902f900fa72da4
Bush-wa-khoi <ct.m1.6.bush@gmail.com>
2024-10-03 20:33:13 +0700

    week 4
    sign up ui added

--- main.py
+++ main.py
@@ -1,6 +1,43 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget # them widget tu pyqt6
 from PyQt6 import uic # them chuc nang load ui
 import sys # them thu vien dieu khien he thong
+# Lop dang ky
+class SignUp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("Ui-GiaoDien/signup.ui", self)
+        # them su kien khi nhan vao nut dang ky
+        self.btnSignUp.clicked.connect(self.registerAccount)
+
+    def registerAccount(self):
+        # doc thong tin nguoi dung nhap vao 
+        username = self.txtEmail
+        password = self.txtpassword
+        comfirmPassWord = self.txtcomfirmpassword
+
+        # neu chua nhap thi cho nguoi dung nhap 
+        if username.text() == "":
+            username.setFocus()
+            return
+        if password.text() == "":
+            password.setFocus()
+            return
+        if comfirmPassword.text() == "":
+            confirmPassWord.setFocus()
+            return
+        # neu nhu email khong dung dinh dang
+        if username.text().find("@gmail.com") == -1:
+            username.clear()
+            username.setFocus()
+            return
+        # neu nhu mat khau va nhap lai mat khau thong dung
+        if password.text() != confirmPassword.text():
+            comfirmPassword.setFocus()
+            return
+        # them tai khoan moi vao file
+        with open("accounts.txt", "a") as file:
+             file.write(username.text() + ":" + password.text() + "\n")
+        self.close
 
 class Login(QMainWindow):
     # ham khoi tao giao dien UI
@@ -10,11 +47,22 @@ class Login(QMainWindow):
         uic.loadUi("Ui-GiaoDien/login.ui", self)
         self.btnLogin.clicked.connect(self.checkLogin)
     def checkLogin(self):
-        if self.txtemail.text() == "DoMinhKhoi" and self.txtpassword.text() == "123":
+        # mo file txt len de doc thong tin
+        with open("accounts.txt", "r") as file:
+            data = file.readlines()
+            accounts = []
+            for line in data:
+                line = line.replace("\n", "")
+                line = line.strip()
+                accounts.append(line)
+            
+        if self.txtemail.text() + ":" + self.txtpassword.text() in accounts:
             self.close()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     lg = Login()
-    lg.show()
+    su = SignUp()
+    su.show()
+    # lg.show()
     app.exec()
